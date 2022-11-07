21f176fed39f2888f482805c48961620867d07f1
nilesh1996b <nileshbind.nb@gmail.com>
2022-11-08 01:47:20 +0530

    add

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+
+let animate1 = new Typed('#ani',{
+strings:["Full Stack Developer ","Java Backend Developer ","Front End Developer "],
+typeSpeed:100,
+backSpeed:100,
+loop:true,
+})
+
+let animate2 = new Typed('#ani2',{
+strings:["Full Stack Developer ","Java Backend Developer ","Front End Developer "],
+typeSpeed:100,
+backSpeed:100,
+loop:true,
+})
+
